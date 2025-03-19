3e7c1fe026574a6d5a8c2ed9b6f9c70fd94e4d60
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-03-19 22:10:57 +0530

    created authController and updated user schema to store access tokens for session management

--- server/model/index.js
+++ server/model/index.js
@@ -26,6 +26,10 @@ const UserSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'RideRequest',
     default: null
+  },
+  accessToken: {
+    type: String,
+    default: null
   }
 });
 
